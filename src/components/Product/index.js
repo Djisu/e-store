1da3dda860e49b5ecb83f1728aeae5c861ec560e
Paul Fleischer-Djoleto <pfleischer2002@yahoo.co.uk>
2022-03-06 16:48:11 +0000

    commit estore images

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -5,11 +5,20 @@ const Product = () => {
   return (
     <div className="row">
       <div className="col-lg-3 col-md-6">
-        <img
-          src={require('../../assets/img/shop/shop-1.jpg')}
-          alt="product-1"
-          className="img-fluid"
-        />
+        <div className="product__item"> 
+          <img
+            src={require('../../assets/img/shop/shop-1.jpg')}
+            alt="product-1"
+            className="img-fluid"
+          />
+          <ul>
+            <li>
+                <a href={require('../../assets/img/shop/shop-1.jpg')}/>
+                <span className="fa fa-arrows-alt"/>
+            </li>
+          </ul>
+        </div>
+
         <div>
           <h6>
             <a href="#">Furry Hooded</a>
@@ -25,6 +34,7 @@ const Product = () => {
                 $590.00
           </div>
         </div>
+
       </div>
     </div>
   )
